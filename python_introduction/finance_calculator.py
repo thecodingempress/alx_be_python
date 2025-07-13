bdae63792952a50bf86ac6a3cf909c4cafbac523
codyempress <codyempress@gmail.com>
2025-07-13 20:18:51 +0300

    advancved task - financial calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+monIncome = input("Enter your monthly income: ")
+monExpenses = input("Enter your total monthly expenses: ")
+
+monIncome = int(monIncome)
+monExpenses = int(monExpenses)
+
+interest = .05  # Example interest rate of 5%
+monSavings = monIncome - monExpenses
+
+projectedSavings = monSavings * 12 + (monSavings * interest * 12)
+print(f"Your monthly savings are: ${monSavings}.")
+print(f"Projected savings after one year, with interest, is: ${projectedSavings}.")
