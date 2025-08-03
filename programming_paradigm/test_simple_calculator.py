0c37590df27431bcf8108279a0c6c29708a289d2
codyempress <codyempress@gmail.com>
2025-08-03 18:10:24 +0300

    File upload to GitHB

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,19 +5,19 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
         """Set up the SimpleCalculator instance for testing."""
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calculator.add(3, 5), 8)
+        self.assertEqual(self.calc.add(3, 5), 8)
 
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(10, 4), 6)
 
     def test_multiplication(self):
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
 
     def test_division(self):
-        self.assertEqual(self.calculator.divide(8, 2), 4)
+        self.assertEqual(self.calc.divide(8, 2), 4)
 
     def test_divide_by_zero(self):
-        self.assertIsNone(self.calculator.divide(5, 0))
+        self.assertIsNone(self.calc.divide(5, 0))
