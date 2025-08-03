6374b2cc7341a0d552701cb64999fcd62a2aed1c
codyempress <codyempress@gmail.com>
2025-08-03 12:52:21 +0300

    Changed 0 to 'False' and 1 to 'True'

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
             self.account_balance -= amount
             return True
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
