b11db5252690afc437926d231ffe0f7363c6d6cf
codyempress <codyempress@gmail.com>
2025-08-03 15:35:48 +0300

    File upload to GitHB

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,28 @@
+
+
+from unittest import case
+
+
+def safe_divide(numerator, denominator):
+    """
+    Safely divides two numbers, handling division by zero.
+    """
+
+    try:
+        if not isinstance(numerator, (int, float)):
+            numerator = float(numerator)
+
+        if not isinstance(denominator, (int, float)):
+            denominator = float(denominator)
+
+        result = numerator / denominator
+    
+    except ZeroDivisionError:
+        return("Error: Cannot divide by zero")
+    
+    except ValueError:
+        return("Error: Please enter numeric values only.")
+    
+    else:
+        return result
+    
