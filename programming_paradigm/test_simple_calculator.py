62ef9817ea52d29a3aa95f776cc8a59f3937b4a7
codyempress <codyempress@gmail.com>
2025-08-03 17:47:43 +0300

    Unit tests for calculator function

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,23 +1,23 @@
 import unittest
-from simple_calculator import SimpleCalculator
+from simple_calculator import SimpleCalculator()
 
 class TestSimpleCalculator(unittest.TestCase):
     def test_add(self):
-        result = SimpleCalculator.add(3, 5)
+        result = SimpleCalculator().add(3, 5)
         self.assertEqual(result, 8)
     
     def test_subtract(self):
-        result = SimpleCalculator.subtract(10, 4)
+        result = SimpleCalculator().subtract(10, 4)
         self.assertEqual(result, 6)
 
     def test_multiply(self):
-        result = SimpleCalculator.multiply(2, 3)
+        result = SimpleCalculator().multiply(2, 3)
         self.assertEqual(result, 6)
 
     def test_divide(self):
-        result = SimpleCalculator.divide(8, 2)
+        result = SimpleCalculator().divide(8, 2)
         self.assertEqual(result, 4)
 
     def test_divide_by_zero(self):
-        result = SimpleCalculator.divide(5, 0)
+        result = SimpleCalculator().divide(5, 0)
         self.assertIsNone(result)
